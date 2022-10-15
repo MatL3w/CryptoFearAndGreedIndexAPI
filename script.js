501d06c23b2bfe957996e43fb47399a940a6ac0c
Lecislav <lecislav@protonmail.com>
2022-10-15 23:23:44 +0200

    add setting a limit from input argument

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 //modules
-const https = require('node:https');
+const https = require("node:https");
 const fs = require("node:fs");
 
 //variables
-let data='';
+let str = "";
+let limit = 100;
 
 //CORE
+setLimitFromInputAgruments();
 getRequest(saveDataToFile);
 
 //CORE
 
-
 //FUNCTIONS
-function saveDataToFile(filename,content) {
-      fs.writeFile(filename, content, (err) => {
-        if (err) throw err;
-        console.log("The file has been saved!");
+function setLimitFromInputAgruments(){
+    if(process.argv[2]) limit=Number(process.argv[2]);
+}
+function saveDataToFile(filename, content) {
+  fs.writeFile(filename, content, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+}
+function getRequest(save) {
+  const options = {
+    hostname: "api.alternative.me",
+    port: 443,
+    path: `/fng/?limit=${limit}&format=json&date_format=world`,
+    method: "GET",
+  };
+    https.request(options, (res) => {
+      res.on("data", (chunk) => {
+        str += chunk;
       });
-    
-  }
-function getRequest(save){
-    const options = {
-        hostname: 'api.alternative.me',
-        port: 443,
-        path: '/fng/?limit=2',
-        method: 'GET'
-      };
-
-      https.request(options, (res)=>{
-
-        res.on('data', (chunk)=> {
-          data += chunk;
-        });
-      
-        res.on('end', () => {
-          console.log(data);
-          save('data',data);//save data to file
-        });
-        }).end();
+      res.on("end", () => {
+        save("data", str); //save data to file
+      });
+    }).end();
 }
