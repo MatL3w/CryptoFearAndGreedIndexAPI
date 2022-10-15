fb56475096f7d80717ae75e6cbecc5fdb864e31a
Lecislav <lecislav@protonmail.com>
2022-10-15 22:52:41 +0200

    add function saving data to file

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 //modules
 const https = require('node:https');
+const fs = require("node:fs");
 
 //variables
-let data;
+let data='';
 
 //CORE
-getRequest();
+getRequest(saveDataToFile);
 
 //CORE
 
 
 //FUNCTIONS
-function getRequest(){
+function saveDataToFile(filename,content) {
+      fs.writeFile(filename, content, (err) => {
+        if (err) throw err;
+        console.log("The file has been saved!");
+      });
+    
+  }
+function getRequest(save){
     const options = {
         hostname: 'api.alternative.me',
         port: 443,
@@ -27,6 +35,7 @@ function getRequest(){
       
         res.on('end', () => {
           console.log(data);
+          save('data',data);//save data to file
         });
         }).end();
 }
