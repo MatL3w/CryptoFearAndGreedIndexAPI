f4f18004da352d76fc1d18c5119694b060932d09
Lecislav <lecislav@protonmail.com>
2022-10-16 14:59:55 +0200

    changed format of saved data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //modules
 const https = require("node:https");
 const fs = require("node:fs");
+const { json } = require("stream/consumers");
 
 //variables
 let str = "";
@@ -13,15 +14,15 @@ getRequest(saveDataToFile);
 //CORE
 
 //FUNCTIONS
-function setLimitFromInputAgruments(){
-  for(let i=0;i<process.argv.length;i++){
-    if(typeof process.argv[i]==="number"){
-      limit=process.argv[i];
+function setLimitFromInputAgruments() {
+  for (let i = 0; i < process.argv.length; i++) {
+    if (typeof process.argv[i] === "number") {
+      limit = process.argv[i];
       break;
     }
   }
 
-    if(process.argv[2]) limit=Number(process.argv[2]);
+  if (process.argv[2]) limit = Number(process.argv[2]);
 }
 function saveDataToFile(filename, content) {
   fs.writeFile(filename, content, (err) => {
@@ -36,12 +37,14 @@ function getRequest(saveFunction) {
     path: `/fng/?limit=${limit}&format=json&date_format=world`,
     method: "GET",
   };
-    https.request(options, (res) => {
+  https.request(options, (res) => {
       res.on("data", (chunk) => {
         str += chunk;
       });
       res.on("end", () => {
-        saveFunction("data", str); //save data to file
+        const { data } = JSON.parse(str);
+        saveFunction("data", JSON.stringify(data)); //save data to file
       });
-    }).end();
+    })
+    .end();
 }
